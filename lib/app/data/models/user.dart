b89b5adfe3acb67e3c671af7705a73200bc54d12
Eldar <92878793+eldarheleg@users.noreply.github.com>
2023-01-04 13:48:26 +0100

    app icon, name, model

---
+++ lib/app/data/models/user.dart
@@ -0,0 +1,24 @@
+class User {
+  final int id;
+  final String firstname;
+  final String lastname;
+  final String email;
+  final String password;
+
+  User(
+      {required this.id,
+      required this.firstname,
+      required this.lastname,
+      required this.email,
+      required this.password});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as int,
+      firstname: json['firstname'] as String,
+      lastname: json['lastname'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+  }
+}
